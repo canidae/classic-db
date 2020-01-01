c6a7b1bcb81bcc1460008c75192029d9e666dfa3
Vidar Wahlberg <canidae@exent.net>
2020-01-01 12:53:15 +0100

    changed to practicy dummy. still following player, though

--- Updates/2643_practice_dummy_test.sql
+++ Updates/2643_practice_dummy_test.sql
@@ -1,1 +1,1 @@
-UPDATE creature_template SET unitflags = 262144, healthmultiplier = 1000000 WHERE entry = 4952 OR entry = 5652;
+UPDATE creature_template SET inhabittype = 1, unitflags = 262656, healthmultiplier = 1000000, armor = 0, mechanicimmunemask = 0 WHERE entry = 4952 OR entry = 5652;
