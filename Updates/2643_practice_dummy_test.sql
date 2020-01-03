b74baceca3aa67ea6a139cf53eae4333eb82bd4e
Vidar Wahlberg <canidae@exent.net>
2020-01-03 13:37:05 +0100

    another attempt making practice dummies not move

--- Updates/2643_practice_dummy_test.sql
+++ Updates/2643_practice_dummy_test.sql
@@ -1,1 +1,1 @@
-UPDATE creature_template SET inhabittype = 1, unitflags = 262656, healthmultiplier = 1000000, armor = 0, mechanicimmunemask = 0 WHERE entry = 4952 OR entry = 5652;
+UPDATE creature_template SET inhabittype = 1, unitflags = 512, healthmultiplier = 1000000, armor = 0, mechanicimmunemask = 0, ainame = 'NullAI' WHERE entry = 4952 OR entry = 5652;
