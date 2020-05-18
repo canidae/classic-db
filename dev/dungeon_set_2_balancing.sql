0de2b1583f4a2b02802ea01af75705961544cec5
Vidar Wahlberg <canidae@exent.net>
2020-05-18 22:19:06 +0200

    opened up remaining quests for dungeon tier 2 gear

--- dev/dungeon_set_2_balancing.sql
+++ dev/dungeon_set_2_balancing.sql
@@ -1,4 +1,4 @@
-UPDATE quest_template SET requiredclasses=3 WHERE entry in (8912); -- warrior gear for paladins, alliance only
-UPDATE quest_template SET requiredclasses=68 WHERE entry in (8914); -- hunter gear for shamans, horde only
-UPDATE quest_template SET requiredclasses=400 WHERE entry in (8907, 8909, 8911, 8915, 8916, 8919); -- mage/priest/warlock can decide, both factions
-UPDATE quest_template SET requiredclasses=1032 WHERE entry in (8910, 8917); -- rogue gear for druids, both factions
+UPDATE quest_template SET requiredclasses=3 WHERE entry in (8912, 8937, 8959, 9006); -- heroism for paladins, alliance only
+UPDATE quest_template SET requiredclasses=68 WHERE entry in (8914, 8938, 9017, 9008); -- beastmaster for shamans, horde only
+UPDATE quest_template SET requiredclasses=400 WHERE entry in (8907, 8932, 8953, 9001, 8909, 8934, 8955, 9003, 8911, 8936, 8958, 9005, 8915, 8939, 9018, 9014, 8916, 8940, 9019, 9009, 8919, 8943, 9021, 9012); -- sorcerer's/virtuous/deathmist for mage/priest/warlock, both factions
+UPDATE quest_template SET requiredclasses=1032 WHERE entry in (8910, 8935, 8956, 9004, 8917, 8945, 9020, 9010); -- darkmantle for druids, both factions
