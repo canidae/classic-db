699cc22ac4615bc1dda0c5aa51aa980a03e7367a
Vidar Wahlberg <canidae@exent.net>
2020-05-25 19:41:19 +0200

    attempt at fixing group loot for chests

---
+++ dev/group_loot_chests.sql
@@ -0,0 +1,1 @@
+UPDATE gameobject_template SET data15 = 1 WHERE name IN ("Battered Chest", "Large Battered Chest", "Large Iron Bound Chest", "Large Mithril Bound Chest", "Large Solid Chest", "Solid Chest");
